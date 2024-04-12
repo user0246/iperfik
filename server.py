cddd572227f8322cf4212e9284162b74dc938621
user0246 <166761778+user0246@users.noreply.github.com>
2024-04-13 00:00:33 +0300

    UPDATE

---
+++ server.py
@@ -0,0 +1,12 @@
+import iperf3
+
+port = int(input("Enter port: "))
+
+server = iperf3.Server()
+server.port = port
+n=1
+while True:
+  server.run()
+  print(f"Test completed {n}")
+  n+=1
+print("Test errored")
